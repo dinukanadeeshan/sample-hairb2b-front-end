6c9a3d973be69b472cbf0c43f722be45f4362e38
dinukanadeeshan <dinuka.nadeeshan1993@gmail.com>
2017-12-07 13:13:24 +0530

    -implement header and structure other components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SearchComponent } from './components/search/search.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SearchComponent,
+    BannerComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
